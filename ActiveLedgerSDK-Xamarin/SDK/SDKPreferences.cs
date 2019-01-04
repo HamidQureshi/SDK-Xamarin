a462f14966837e1f6c596df2eb2ea48381f444ac
Hamid Mehmood <HamidQureshi@users.noreply.github.com>
2019-01-04 16:08:01 +0000

    Update SDKPreferences.cs

--- ActiveLedgerSDK-Xamarin/SDK/SDKPreferences.cs
+++ ActiveLedgerSDK-Xamarin/SDK/SDKPreferences.cs
@@ -7,6 +7,7 @@ namespace ActiveLedgerLib
     {
         public static string url;
 
+        // this method can be used for transaction endpoint on the ledger
         public static void setConnection(string protocol, string address, string port)
         {
             url = protocol + "://" + address + ":" + port;
